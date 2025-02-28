65b1b9a16b98b063b4d01b526c6911f0e8b65a82
Muhammad Usman <usmank.personal@outlook.com>
2025-03-01 04:04:02 +0500

    Made some changes in the format of code

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+# ***** CONFIGURATION *****
+BOT_PREFIX = "!"
+
+
+# ***** CONSTANTS *****
 EMBED_COLOR_CODE = "#2f3136"
+
+
+# ***** FILE PATHS *****
+PERMISSIONS_JSON_FILE_PATH = "./database/permissions.json"
+DATABASE_FILE_PATH = "./database/database.db"
--- main.py
+++ main.py
@@ -4,35 +4,40 @@ from dotenv import load_dotenv
 
 from functions.database import database
 
+from config import BOT_PREFIX
+from config import DATABASE_FILE_PATH
+from config import PERMISSIONS_JSON_FILE_PATH
+
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=BOT_PREFIX, intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("🟡 | Bot is starting")
 
     print("🟡 | Checking permissions")
-    if not os.path.exists("./database/permissions.json"):
+    if not os.path.exists(PERMISSIONS_JSON_FILE_PATH):
         print("🔴 | Permissions file not found")
         print("🟡 | Creating permissions file")
-        with open("./database/permissions.json", "w") as f:
+        with open(PERMISSIONS_JSON_FILE_PATH, "w") as f:
             f.write("{}")
         print("🟢 | Permissions file created")
 
 
     print("🟡 | Connecting to database")
-    db = database('./database/database.db')
+    db = database(DATABASE_FILE_PATH)
+    print("🟢 | Connected to database")
 
     print("🟡 | Loading all extensions")
     await bot.load_extension("extensions.Permissions")
+    print("🟢 | All extensions loaded")
 
-
-    
-    
-
+    print("🟡 | Syncing tree")
     await bot.tree.sync()
+    print("🟢 | Tree synced")
+
     print("🟢 | Bot is live")
 
 
